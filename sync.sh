63fb0a8dc4dca4efbe1c96dfbe995aa4f4ea6569
Xackery Xtal <xackery@gmail.com>
2017-07-10 15:10:44 -0700

    Added fixes to hugo

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 echo "Generating hugo"
 hugo
 echo "Syncing public/ with xackery.com..."
-aws s3 sync public/ s3://xackery.com --acl public-read --region us-west-2 --delete
+aws s3 sync public/ s3://xackery.com --acl public-read --region us-west-2 --delete --profile xackery
